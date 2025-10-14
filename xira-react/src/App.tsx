f93e45053375ebd3ad0712ca4bb9c0af3d103ca0
grtthealex <grtthealex@gmail.com>
2025-10-14 17:42:04 +0800

    added file upload display

--- xira-react/src/App.tsx
+++ xira-react/src/App.tsx
@@ -3,19 +3,33 @@ import './App.css'
 
 function App() {
   const [file, setFile] = useState<File | null>(null); //barebones for now..
-
+  const OnChange = (files) => {
+    const uploadedFile = files.target.files[0];
+    setFile(uploadedFile);  
+}
   return (
     <>
-      <h1>PE Malware Analysis</h1>
+    <h1>PE Malware Analysis</h1>
       <div className = "upload_form">
         <input 
           type = "file" 
           className = "file_input" 
           id = "file_input" 
           accept = ".exe"
+          onChange = {OnChange}
         />
-        <label htmlFor = "file_input"> Upload File </label>
+        <label  
+          htmlFor = "file_input"
+          className = "file_hover"> Upload File </label>
         <p className = "msg">*Only supports a portable exectuable file</p>
+        {file && (
+          <>
+          <div className = "uploaded_files">
+            <p className = "file_name"> {file.name} </p>
+            <p className = "file_size"> {(file.size / 1048576).toFixed(2)}mb </p>
+           </div>
+           </>
+        )}
         <button className = "upload_btn">Analyze</button>
       </div>
     </>
